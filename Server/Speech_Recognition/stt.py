e2842bf41ee49bc614812268b7f9cc3597b5fb97
Arjun31415 <arjunp0710@gmail.com>
2021-12-08 20:58:45 +0530

    Started server side speech recognition

--- Server/Speech_Recognition/stt.py
+++ Server/Speech_Recognition/stt.py
@@ -4,14 +4,19 @@ import librosa
 import numpy as np
 from scipy.io import wavfile
 from IPython.display import Audio
+from os.path import dirname, join as pjoin
+from os import getcwd
 from transformers import Wav2Vec2ForCTC, Wav2Vec2Tokenizer, Wav2Vec2CTCTokenizer, Wav2Vec2Processor
 tokenizer = Wav2Vec2Processor.from_pretrained("facebook/wav2vec2-base-960h")
 model = Wav2Vec2ForCTC.from_pretrained("facebook/wav2vec2-base-960h")
 
-file_name = '2021-12-08-16-01-31.wav'
+print(getcwd())
+data_dir = pjoin(getcwd(), "./Server")
+file_name = pjoin(data_dir, 'temp.wav')
 data = wavfile.read(file_name)
 
 framerate = data[0]
+print(framerate)
 sounddata = data[1]
 time = np.arange(0, len(sounddata))/framerate
 input_audio, _ = librosa.load(file_name, sr=16000)
